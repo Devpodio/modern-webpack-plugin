876a7ded03e9756b4d9455350dcb12b00533484e
unibtc <unibtc@gmail.com>
2018-11-16 22:45:31 +0000

    require html-webpack-plugin

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  ** https://github.com/vuejs/vue-cli/blob/dev/packages/@vue/cli-service/lib/webpack/ModernModePlugin.js
  */
 const EventEmitter = require('events')
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 // https://gist.github.com/samthor/64b114e4a4f539915a95b91ffd340acc
 const safariFix = `!function(){var e=document,t=e.createElement("script");if(!("noModule"in t)&&"onbeforeload"in t){var n=!1;e.addEventListener("beforeload",function(e){if(e.target===t)n=!0;else if(!e.target.hasAttribute("nomodule")||!n)return;e.preventDefault()},!0),t.type="module",t.src=".",e.head.appendChild(t),t.remove()}}();`
 
